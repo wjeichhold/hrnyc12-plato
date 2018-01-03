2e2e6c66445d98dec50196c4a382b7b44882b6ae
rvcwhitworth <rvcwhitworth@gmail.com>
2018-01-03 11:35:22 -0500

    (feat) Add create event form

--- react-client/src/components/CreateEvent.jsx
+++ react-client/src/components/CreateEvent.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-const CreateEvent = (props) => (
-  <div>
-    <h4> Create Event </h4>
-  </div>
-)
+class CreateEvent extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      eventName: '',
+      location: {
+        longitude: '',
+        latitude: '',
+      },
+      time: '',
+      attendees: []
+    }
+  }
+  
+  handleEventNameChange (e) {
+    this.setState({
+      eventName: e.target.value
+    });
+  }
+
+  handleTimeChange (e) {
+    this.setState({
+      time: e.target.value
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h4> Create Event </h4>
+        <form>
+          <label>
+            Event Name:
+            <input type="text" value={this.state.eventName} onChange={this.handleEventNameChange} />
+          </label>
+          <br/>
+            To be added: MapWithSearchBox component
+            { /* <MapWithSearchBox /> */ }
+          <br/>
+          <label>
+            Time:
+            <input type="time" value={this.state.time} onChange={this.handleTimeChange} />
+          </label>
+          <br/>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CreateEvent;
