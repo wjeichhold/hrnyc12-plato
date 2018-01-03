811b0d63decc81843019b5e60baf65d1027e503a
rvcwhitworth <rvcwhitworth@gmail.com>
2018-01-03 11:50:18 -0500

    (feat) Add axios call to create event form

--- react-client/src/components/CreateEvent.jsx
+++ react-client/src/components/CreateEvent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class CreateEvent extends React.Component {
   constructor (props) {
@@ -12,6 +13,9 @@ class CreateEvent extends React.Component {
       time: '',
       attendees: []
     }
+    this.handleEventNameChange = this.handleEventNameChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   handleEventNameChange (e) {
@@ -26,24 +30,37 @@ class CreateEvent extends React.Component {
     });
   }
 
+  handleSubmit (e) {
+    e.preventDefault();
+    console.log('Submitting, contents of state:', this.state);
+    axios.post('/event', this.state).then((response) => console.log(response));
+  }
+
   render () {
     return (
       <div>
-        <h4> Create Event </h4>
-        <form>
+        <h4> Event Details </h4>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Event Name:
-            <input type="text" value={this.state.eventName} onChange={this.handleEventNameChange} />
+            <input type="text" value={this.state.eventName} onChange={this.handleEventNameChange} required/>
           </label>
           <br/>
+
             To be added: MapWithSearchBox component
             { /* <MapWithSearchBox /> */ }
+
           <br/>
           <label>
             Time:
-            <input type="time" value={this.state.time} onChange={this.handleTimeChange} />
+            <input type="time" value={this.state.time} onChange={this.handleTimeChange} required/>
           </label>
           <br/>
+
+          To be added: AddAttendee and AttendeeList components
+
+          <br/>
+          <input type="submit" value="Create Event" />
         </form>
       </div>
     );
