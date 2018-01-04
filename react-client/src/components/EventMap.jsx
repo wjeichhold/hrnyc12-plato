64aba8fce7a7a20b2314e4294e400bb7913b9ebc
rvcwhitworth <rvcwhitworth@gmail.com>
2018-01-03 21:32:28 -0500

    (feat) Add user geolocation

--- react-client/src/components/EventMap.jsx
+++ react-client/src/components/EventMap.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
+import axios from 'axios';
+import { URLSearchParams } from 'url';
 
-const EventMap = (props) => (
-  <div>
-    <h4> Event Map </h4>
-    <p> Event ID: {props.match.params.number} </p>
-  </div>
-)
+class EventMap extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      eventLocation: {
+        lat: null,
+        lng: null
+      },
+      users: [],
+      userLocation: {
+        lat: '',
+        lng: ''
+      },
+      userId: ''
+    }
+  }
+  
+  componentDidMount () {
+    this.getUserId();
+    this.getUserLocation();
+  }
+
+  sendUserLocation () {
+    console.log('Sending to server:', {
+      userId: this.state.userId,
+      lat: this.state.userLocation.lat,
+      lng: this.state.userLocation.lng
+    });
+
+    axios.put('/users', {
+      userId: this.state.userId,
+      lat: this.state.userLocation.lat,
+      lng: this.state.userLocation.lng
+    })
+    .then((response) => console.log(response));
+  }
+
+  getUserId () {
+    this.setState({
+      userId: this.props.location.search.replace('?userId=', '')
+    });
+  }
+
+  getUserLocation () {
+    const geoSuccess = (position) => {
+      this.setState({
+        userLocation: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      }, this.sendUserLocation);
+    }
+
+    const geoError = (error) => {
+      console.error('Error getting location:', error.message);
+    }
+
+    navigator.geolocation.watchPosition(geoSuccess, geoError);
+  }
+  render () {
+    return (
+      <div>
+        <h4> Event Map </h4>
+        <p> Event ID: {this.props.match.params.number} </p>
+        <p> Current location: {this.state.userLocation.lat + ',' + this.state.userLocation.lng} </p>
+        <p> TODO add dynamic map </p>
+      </div>
+    );
+  }
+}
 
 export default EventMap;
