efb811b1ed980389d80445c2a0b2a67e95a0d701
Bill Eichhold <wjeichhold@gmail.com>
2018-01-15 18:55:20 -0500

    loading page redirects after submit

--- react-client/src/components/LoadingPage.jsx
+++ react-client/src/components/LoadingPage.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 import Paper from 'material-ui/Paper';
 
-const LoadingPage = (props) => {
-  let style = {
-    paper : {
-      margin: 20,
-      textAlign: 'center',
-      display: 'inline-block',
-      padding: 5,
-      marginLeft: '25%'
+class LoadingPage extends React.Component {
+  constructor(props) {
+    super(props)  
+    this.state = {
+      style: {
+        paper : {
+        margin: 20,
+        textAlign: 'center',
+        display: 'inline-block',
+        padding: 5,
+        marginLeft: '25%'
+      }
     }
   }
+}
 
-  return (
-    <Paper style={style.paper} zDepth={2}>
-      <h1>WAYN</h1>
-      <h4>Please wait while we plan your event.  You will receive a text when it is created.</h4>
-      <CircularProgress size={80} thickness={5} />
-    </Paper>
-  )
+  componentDidMount() {
+    setTimeout(() => {this.props.history.push('/')}, 3000)
+  }
+
+  render() {
+      return (
+      <Paper style={this.state.style.paper} zDepth={2}>
+        <h1>WAYN</h1>
+        <h4>Please wait while we plan your event.  You will receive a text when it is created.</h4>
+        <CircularProgress size={80} thickness={5} />
+      </Paper>
+    )  
+  }
+  
 };
 
 export default LoadingPage;
