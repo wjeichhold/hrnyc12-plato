11e8fe088de3f2e20fb5efad32f85587fb9a89fb
Bill Eichhold <wjeichhold@gmail.com>
2018-01-19 20:00:38 -0500

    obscuring keys and fixing nulls in db

--- react-client/src/components/LandingPage.jsx
+++ react-client/src/components/LandingPage.jsx
@@ -4,6 +4,7 @@ import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import FacebookLogin from 'react-facebook-login';
+import fbKey from '../../../config.js';
 
 class LandingPage extends React.Component {
   constructor (props) {
@@ -59,6 +60,7 @@ class LandingPage extends React.Component {
   }
 
   render () {
+    console.log(fbKey)
     const style = {
       // height: 100,
       // width: 100,
@@ -72,7 +74,7 @@ class LandingPage extends React.Component {
 
     if(this.state.facebook === ''){
       return(<FacebookLogin
-    appId="143607479688685"
+    appId={fbKey.facebookId}
     autoLoad={true}
     fields="name,email,picture"
     onClick={this.responseFacebook}
